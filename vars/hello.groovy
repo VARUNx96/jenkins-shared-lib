b47193b0342ca2a2ec42baadd752dcf7e980313a
VARUN J <70467762+VARUNx96@users.noreply.github.com>
2026-01-08 13:15:17 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,3 +1,8 @@
 def call() {
   echo "hello guys this is my first shared lib"
+  echo "this if from github...🔥"
+  echo "        |              "
+  echo "        W              "
+  echo "this is delivered to jenkins"
+  echo "😭 finally.....🔥"
 }
