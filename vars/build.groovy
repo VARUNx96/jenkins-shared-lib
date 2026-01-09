d44cfa68b93653f24b2fa8693393c4a7193f48e7
VARUN J <70467762+VARUNx96@users.noreply.github.com>
2026-01-09 11:33:19 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,8 +1,8 @@
 def call(String docker_image) {
                 echo "this is building the code....🧱🧱🧱"
                 sh """
-                  export PATH=/usr/local/bin:/opt/homebrew/bin:$PATH
-                  echo "PATH is: $PATH"
+                  export PATH=/usr/local/bin:/opt/homebrew/bin:\$PATH
+                  echo "PATH is: \$PATH"
                   docker --version
                   docker build -t ${docker_image} .
                 """
